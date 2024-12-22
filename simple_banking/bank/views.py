0f370e9da9cecc7197121d79d7151a60a3260bd1
kevinreal45 <kevin.ndyabandiho@strathmore.edu>
2024-12-23 02:35:29 +0300

    changes were made to reflect the working of the app

--- simple_banking/bank/views.py
+++ simple_banking/bank/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Account, Transaction, Card, Customer, Loan, User  # Import all models
+from .serializers import AccountSerializer, TransactionSerializer, CardSerializer, CustomerSerializer, LoanSerializer, UserSerializer  # Import all serializers
+from rest_framework import viewsets
 
-# Create your views here.
+# Remove function-based views and keep only the viewsets
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
